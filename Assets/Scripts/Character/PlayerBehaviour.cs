8e915a1742dc2febaee1a87ab938fb156f369b88
benjaminwcfrank <benjaminwcfrank@gmail.com>
2023-01-21 15:54:04 -0500

    Added Lab1 Scripts. Got Char hooked up and working.

--- Assets/Scripts/Character/PlayerBehaviour.cs
+++ Assets/Scripts/Character/PlayerBehaviour.cs
@@ -4,25 +4,56 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public Transform groundedPoint;
-    public float groundedRadius;
-    public CharacterController charController;
+    public CharacterController controller;
+
+    [Header("Movement Properties")]
+    public float maxSpeed = 10.0f;
+    public float gravity = -30.0f;
+    public float jumpHeight = 3.0f;
+    public Vector3 velocity;
+
+    [Header("Ground Detection Properties")]
+    public Transform groundPoint;
+    public float groundRadius = 0.5f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundPoint.position, groundRadius, groundMask);
+
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * maxSpeed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(groundedPoint.position, groundedRadius);
+        Gizmos.DrawWireSphere(groundPoint.position, groundRadius);
     }
 }
