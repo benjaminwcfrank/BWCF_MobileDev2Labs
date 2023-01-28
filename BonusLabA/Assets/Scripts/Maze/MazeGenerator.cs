71fa7f867949fe7145e4f05be5e5dd231747ae08
benjaminwcfrank <benjaminwcfrank@gmail.com>
2023-01-28 18:07:05 -0500

    MazeGenerator now spawns an 8x8 grid of tiles

--- BonusLabA/Assets/Scripts/Maze/MazeGenerator.cs
+++ BonusLabA/Assets/Scripts/Maze/MazeGenerator.cs
@@ -10,8 +10,8 @@ public class MazeGenerator : MonoBehaviour
 
     public Transform tileParent;
 
-    public GameObject[] tileArray;
-    public List<GameObject> tileList;
+    private GameObject[] tileArray;
+    private List<GameObject> tileList;
     public GameObject startTile;
     public GameObject endTile;
 
@@ -23,10 +23,11 @@ public class MazeGenerator : MonoBehaviour
         startTile = Resources.Load<GameObject>("Tiles/StartTile");
         endTile = Resources.Load<GameObject>("Tiles/EndTile");
 
-        tileArray = new GameObject[3];
+        tileArray = new GameObject[4];
         tileArray[0] = Resources.Load<GameObject>("Tiles/BarnTile");
         tileArray[1] = Resources.Load<GameObject>("Tiles/FactoryTile");
         tileArray[2] = Resources.Load<GameObject>("Tiles/EmptyTile");
+        tileArray[3] = Resources.Load<GameObject>("Tiles/EmptyTile");
 
         tileList = new List<GameObject>();
          
@@ -42,9 +43,9 @@ public class MazeGenerator : MonoBehaviour
         {
             for (int coll = 0; coll < mazeWidth; coll++)
             {
-               // var tempTile = Instantiate(tileArray[Random.Range(0, 3)])
+                var tempTile = Instantiate(tileArray[Random.Range(0, 4)], new Vector3(coll * 16, 0.0f, row * 16), Quaternion.Euler(0.0f, Random.Range(1, 4) * 90, 0.0f), tileParent);
 
-
+                tileList.Add(tempTile);
 
 
             }
